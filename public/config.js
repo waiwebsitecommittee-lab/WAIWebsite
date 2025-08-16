fecafc8b62cc37ea05a59fc341c6dea6482ec324
waiwebsitecommittee-lab <waiwebsitecommittee@gmail.com>
2025-08-16 18:31:37 -0400

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 export default {
     title: 'Area 71 District 38 Meeting Finder',
     apiURL: './data/meetings.json',
-    region: 'Williamsburg Virginia'
-    timeZone: 'US/Eastern',
+    region: 'Williamsburg Virginia',
+    timeZone: 'America/New_York', 
     theme: 'default', // or dark
     mapTiles: {
 	url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
